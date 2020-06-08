0e6d86b06e906e5d55ef757b26538a7d9d4e650d
Kit Kogan <kkogan.dev@gmail.com>
2020-06-08 17:00:56 -0500

    add text key prop

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default function App() {
         <Button title="Add" onPress={addGoalHandler} />
       </View>
       <View>
-        {courseGoals.map((goal) => <Text>{goal}</Text>)}
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );

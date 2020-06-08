1ce23f626c4610a7bf4d94ba93334d86223768f5
Kit Kogan <kkogan.dev@gmail.com>
2020-06-08 16:31:15 -0500

    set up hooks to set state on user input

--- App.js
+++ App.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  return (
-   <View style={{padding: 30}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput placeholder="Course Goals" style={{ borderColor: 'black', borderWidth: 1, padding: 10, width: '80%' }} />
-        <Button title="Add" />
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState('');
 
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
+
+  return (
+   <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+        placeholder="Course Goals" 
+        style={styles.input} 
+        onChangeText={goalInputHandler}
+        value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
+      <View />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+  },
+  input: {
+     borderColor: 'black', 
+     borderWidth: 1, 
+     padding: 10, 
+     width: '80%' 
+  }
 });
